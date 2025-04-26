24229ff53a3df534b51dc91ead00a85dbcfc2a74
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-26 20:49:46 -0300

    feat: add colored console logging using colors package

--- rest/nodejs-express-traversymedia/server.js
+++ rest/nodejs-express-traversymedia/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import posts from "./routes/posts.js"
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
 const port = process.env.PORT || 8080
 
 const app = express();
@@ -17,7 +18,7 @@ app.use(logger)
 app.use("/api/posts", posts)
 
 // Error handler
+app.use(notFound)
 app.use(errorHandler)
-
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
