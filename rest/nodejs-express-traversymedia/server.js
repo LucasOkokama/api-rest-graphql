487e3a0d3e946ff0185483bf06fcad9c34b95aec
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-26 21:15:34 -0300

    chore: add logic to get __dirname using import.meta.url

--- rest/nodejs-express-traversymedia/server.js
+++ rest/nodejs-express-traversymedia/server.js
@@ -1,9 +1,15 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from "url";
 import posts from "./routes/posts.js"
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/error.js";
 import notFound from "./middleware/notFound.js";
 const port = process.env.PORT || 8080
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express();
 
@@ -13,6 +19,8 @@ app.use(express.urlencoded({ extended: false }))
 
 // Logger middleware
 app.use(logger)
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use("/api/posts", posts)
