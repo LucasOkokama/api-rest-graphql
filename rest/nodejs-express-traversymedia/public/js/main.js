ca842af4312489bdd43829b05e318227b5849b14
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-26 21:48:26 -0300

    feat: add functionality to create new post via form submission

--- rest/nodejs-express-traversymedia/public/js/main.js
+++ rest/nodejs-express-traversymedia/public/js/main.js
@@ -1,8 +1,8 @@
 const output = document.querySelector("#output")
 const button = document.querySelector("#get-posts-btn")
+const form = document.querySelector("#add-post-form")
 
 async function showPosts() {
-
   try {
     const res = await fetch('http://localhost:8000/api/posts')
     if (!res.ok) {
@@ -23,4 +23,30 @@ async function showPosts() {
   }
 }
 
+async function addPost(e) {
+  e.preventDefault()
+  const formData = new FormData(this)
+  const title = formData.get("title")
+
+  try {
+    const res = await fetch("http://localhost:8000/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title })
+    })
+
+    if (!res.ok) {
+      throw new Error("Failed to add post")
+    }
+
+    showPosts()
+  }
+  catch (error) {
+    console.error("Error adding post:", error)
+  }
+}
+
 button.addEventListener("click", showPosts)
+form.addEventListener("submit", addPost)
