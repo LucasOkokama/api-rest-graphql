e66951d0b6f4c559eb72d2f9c370f9b6c1b53c9f
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-27 22:21:57 -0300

    feat: add database schema with users and todos tables

--- rest/nodejs-sqlite-smoljames/src/server.js
+++ rest/nodejs-sqlite-smoljames/src/server.js
@@ -1,6 +1,17 @@
 import express from "express"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+// Middleware
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "..", "public")))
+
 
 app.listen(PORT, () => { console.log(`Server has stated on port: ${PORT}`) })
