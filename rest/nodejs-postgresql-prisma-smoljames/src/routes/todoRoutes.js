78627b645a92980defb75f0766d1f5762db51075
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-05-01 21:27:11 -0300

    refactor: migrate from sqlite to prisma for auth and todo routes

--- rest/nodejs-postgresql-prisma-smoljames/src/routes/todoRoutes.js
+++ rest/nodejs-postgresql-prisma-smoljames/src/routes/todoRoutes.js
@@ -1,37 +1,52 @@
 import express from "express";
 import db from "../db.js";
+import prisma from "../prismaClient.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`);
-  const todos = getTodos.all(req.userID);
+router.get("/", async (req, res) => {
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: req.userId,
+    },
+  });
   res.json(todos);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { task } = req.body;
-  const insertTodo = db.prepare(
-    `INSERT INTO todos (user_id, task) VALUES (?, ?)`
-  );
-  const result = insertTodo.run(req.userID, task);
-  res.json({ id: result.lastInsertRowid, task, completed: 0 });
+  const todo = await prisma.todo.create({
+    data: {
+      task,
+      userId: req.userID,
+    },
+  });
+  res.json(todo);
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { completed } = req.body;
   const { id } = req.params;
-  const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`);
-  updatedTodo.run(completed, id);
-  res.json({ message: "Todo completed" });
+  const updatedTodo = await prisma.todo.update({
+    where: {
+      id: parseInt(id),
+      userId: req.userID,
+    },
+    data: {
+      completed: !!completed,
+    },
+  });
+  res.json(updatedTodo);
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const deletedTodo = db.prepare(
-    `DELETE FROM todos WHERE id = ? AND user_id = ?`
-  );
-  deletedTodo.run(id, req.userID);
+  await prisma.todo.delete({
+    where: {
+      id: parseInt(id),
+      userId: req.userID,
+    },
+  });
   res.json({ meessage: "Todo deleted successfully" });
 });
 
